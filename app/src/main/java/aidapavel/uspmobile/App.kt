2e4804aeda844f43cb05c6f0c834f8b21a2d1ff3
KilliLogan <ivanov-p-a@bk.ru>
2022-02-28 00:47:23 +0300

    Added and activated bottom navigation view Hide the status bar

--- app/src/main/java/aidapavel/uspmobile/App.kt
+++ app/src/main/java/aidapavel/uspmobile/App.kt
@@ -1,12 +1,38 @@
 package aidapavel.uspmobile
 
+import aidapavel.uspmobile.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class App : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_nav_smartphone -> {
+                    Log.d("menuItem", "${menuItem.title}")
+                    true
+                }
+                R.id.bottom_nav_tablet -> {
+                    Log.d("menuItem", "${menuItem.title}")
+                    true
+                }
+                R.id.bottom_nav_accessories -> {
+                    Log.d("menuItem", "${menuItem.title}")
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_smartphone
     }
 }

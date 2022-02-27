b125aab72ef3f0d72aa8af157fcb03d0d362300e
KilliLogan <ivanov-p-a@bk.ru>
2022-02-27 22:29:48 +0300

    Initial commit

---
+++ app/src/main/java/aidapavel/uspmobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package aidapavel.uspmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
